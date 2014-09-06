9b920b9c0f29a07f8acea6c8d98875c8671e06e0
Jay Chopra <jchopra@andrew.cmu.edu>
2014-09-06 18:31:48 -0400

    styling form for application

--- RushAKPsi/urls.py
+++ RushAKPsi/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from RushAKPsi import settings
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'', include('portal.urls')),
-)
+)  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 urlpatterns += patterns('',
