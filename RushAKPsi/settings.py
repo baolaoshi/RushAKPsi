15d2eccefb8a4e8954d7769dbecbdec0515eedc0
Jay Chopra <jchopra@andrew.cmu.edu>
2014-09-07 03:43:18 -0400

    jsing resume button

--- RushAKPsi/settings.py
+++ RushAKPsi/settings.py
@@ -83,10 +83,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Directory for templates
 
