e3b5bc2db10d4100af6022e57beb5494f4452602
renzobautista <renzo@cmu.edu>
2014-09-08 00:52:48 -0400

    validations and splash

--- portal/views.py
+++ portal/views.py
@@ -11,7 +11,7 @@ def index_view(request):
 	data = {}
 	return render(request, 'index.html', data)
 
-def application_view(request):
+def application_view(request, splash=False):
 	if not request.user.is_authenticated():
 		return HttpResponseRedirect("/login")
 
@@ -20,11 +20,30 @@ def application_view(request):
 		
 	data = {}
 
+	if splash:
+		data['splash'] = True
+		rushee = Rushee.objects.get(user=request.user)
+		form = NewRusheeForm(initial={'first_name' : rushee.first_name, 
+									  'last_name' : rushee.last_name,
+									  'phone_num' : rushee.phone_num,
+									  'dorm' : rushee.dorm,
+									  'grad_class' : rushee.grad_class,
+									  'major' : rushee.major,
+									  'gpa' : rushee.gpa,
+									  'q1' : rushee.q1, 
+									  'q2' : rushee.q2,
+									  'q3' : rushee.q3,
+									  'q4' : rushee.q4})	
+		data["form"] = form
+		return render(request, 'application.html', data, context_instance=RequestContext(request))
+
 	if request.method == 'POST':
 		form = NewRusheeForm(request.POST, request.FILES)
 		if form.is_valid():
 			cd = form.cleaned_data
 			rushee = Rushee.objects.get(user=request.user)
+			rushee.first_name = cd['first_name']
+			rushee.last_name = cd['last_name']
 			rushee.phone_num = cd['phone_num']
 			rushee.dorm = cd['dorm']
 			rushee.grad_class = cd['grad_class']
@@ -37,7 +56,7 @@ def application_view(request):
 			rushee.picture = cd['picture']
 			rushee.resume = cd['resume']
 			rushee.save()
-			return HttpResponseRedirect("/application")
+			return application_view(request, splash=True)
 		else:
 			rushee = Rushee.objects.get(user=request.user)
 			form = NewRusheeForm(initial={'phone_num' : rushee.phone_num,
@@ -63,7 +82,6 @@ def application_view(request):
 									  'q3' : rushee.q3,
 									  'q4' : rushee.q4})	
 	data["form"] = form
-	data["rushee"] = Rushee.objects.get(user=request.user)
 	return render(request, 'application.html', data, context_instance=RequestContext(request))
 
 def login_view(request):
@@ -83,24 +101,25 @@ def login_view(request):
 					return HttpResponseRedirect("/application/")
 				else: 
 					data['error'] = "Your email and password don't match!"
-					data['form'] = LoginForm()
+					data['form'] = form
 		else:
 			data['form'] = LoginForm()
 	return render(request,'login.html', data, context_instance=RequestContext(request))
 
 def signup_view(request):
 	data = {}
-	form = NewUserForm(request.POST)
-	if form.is_valid():
-		new_user = form.save()
-		print form.cleaned_data['password1']
-		new_rushee = Rushee(user=new_user)
-		new_rushee.save()
-		new_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
-		if new_user is not None:
-			new_user.backend = 'django.contrib.auth.backends.ModelBackend'
-			login(request, new_user)		
-			return HttpResponseRedirect("/application")
+	if request.method == 'POST':
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			new_user = form.save()
+			print form.cleaned_data['password1']
+			new_rushee = Rushee(user=new_user)
+			new_rushee.save()
+			new_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+			if new_user is not None:
+				new_user.backend = 'django.contrib.auth.backends.ModelBackend'
+				login(request, new_user)		
+				return HttpResponseRedirect("/application")
 	else:
 		form = NewUserForm()
 	data["form"] = form
